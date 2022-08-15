a1ebac29d26820f034ec63b78ee221cd8dcc119e
guguducken <1173781832@qq.com>
2022-08-15 12:09:12 +0800

    test init

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const accseeToken = core.getInput("action_token");
+
+async function run() {
+    let oc = github.getOctokit(accseeToken);
+
+    const repo = github.context.repo;
+
+    const prNum = github.context.payload?.pull_request.number;
+
+    if (prNum === undefined) {
+        return;
+    }
+
+    const { data: pr } = oc.rest.pulls.get(
+        {
+            ...repo,
+            prNum,
+        }
+    )
+    core.info(JSON.stringify(pr));
+}
+
+run();
