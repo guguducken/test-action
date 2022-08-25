cc25da9517a97b735d7eb967a12aa45832708dab
guguducken <1173781832@qq.com>
2022-08-25 16:23:42 +0800

    optimize logic

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const http = require('@actions/http-client');
 
 const accseeToken = core.getInput("action-token");
 
+const test_name = core.getInput("name-test");
+
 async function run() {
     let oc = github.getOctokit(accseeToken);
 
@@ -15,46 +17,11 @@ async function run() {
         return;
     }
 
-    const { data: workflows_all } = await oc.rest.actions.listRepoWorkflows(
-        {
-            ...github.context.repo
-        }
-    )
+    core.info(test_name);
 
-    for (const workflow of workflows_all.workflows) {
-        if (workflow.state == "active") {
-            core.info("Start finding workflow, name is: " + workflow.name);
-            let num_page = 1;
-            while (true) {
-                const { data: { total_count: total_count_1, workflow_runs: workflow_runs_1 } } = await oc.rest.actions.listWorkflowRuns(
-                    {
-                        ...github.context.repo,
-                        workflow_id: workflow.id,
-                        per_page: 100,
-                        page: num_page,
-                        event: "pull_request"
-                    }
-                );
-                for (const workflow_run of workflow_runs_1) {
-                    core.info(workflow_run.event);
-                }
-                core.info("----------------------------------------------");
-                const { data: { total_count: total_count_2, workflow_runs: workflow_runs_2 } } = await oc.rest.actions.listWorkflowRuns(
-                    {
-                        ...github.context.repo,
-                        workflow_id: workflow.id,
-                        per_page: 100,
-                        page: num_page,
-                        event: "pull_request_target"
-                    }
-                );
-                for (const workflow_run of workflow_runs_2) {
-                    core.info(workflow_run.event);
-                }
-                core.info("----------------------------------------------");
-            }
-        }
-    }
+    core.info(JSON.stringify(test_name));
+
+
 }
 
 run();
