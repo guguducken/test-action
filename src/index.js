6a3da7e78dcc50c39567f61b49e5dc508d9850b6
guguducken <1172781832@qq.com>
2022-11-07 06:16:14 +0000

    test

--- src/index.js
+++ src/index.js
@@ -77,14 +77,14 @@ async function getPRTime(issue) {
     }
   }
 }`;
-    let { repository: { issue } } = await oc.graphql(query, {
+    let { issue: issue_a } = await oc.graphql(query, {
         "repo": repo.repo,
         "repo_owner": repo.owner,
         "number_iss": issue.number,
         "First": 100,
         "Skip": 0
     });
-    let edges = issue.timelineItems.edges;
+    let edges = issue_a.timelineItems.edges;
     for (let i = 0; i < edges.length; i++) {
         const e = edges[i];
         if (e.node === undefined || e.node.source === undefined || Object.keys(e.node).length == 0 || Object.keys(e.node.source).length == 0) {
