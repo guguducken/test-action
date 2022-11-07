07ef38340c9565f50dfe3634285498d0be7a4420
guguducken <1172781832@qq.com>
2022-11-07 03:27:36 +0000

    test

--- src/index.js
+++ src/index.js
@@ -10,31 +10,6 @@ const repo = github.context.repo;
 
 async function run() {
 
-
-    // const prNum = github.context.payload?.pull_request.number;
-
-    // if (prNum === undefined) {
-    //     return;
-    // }
-
-    // const { data: pr } = await oc.rest.pulls.get(
-    //     {
-    //         repo: "matrixone",
-    //         owner: "matrixorigin",
-    //         pull_number: 4742
-    //     }
-    // );
-
-    // core.info(pr.body);
-    // core.info(JSON.stringify(pr.body));
-    // core.info("-------------------------------------------------------");
-
-    // const index = pr.body.lastIndexOf("## What this PR does / why we need it:");
-    // core.info(pr.body.substring(index));
-    // core.info(index);
-
-    // core.info(pr.title);
-    // core.info(pr.title.length);
     for (let i = 1; i < 3; i++) {
         const iss = await getIssues(i, 100)
         if (iss === undefined) {
@@ -45,31 +20,11 @@ async function run() {
                 if (e.pull_request !== undefined) {
                     core.info("This is PR, " + e.title + " " + e.created_at);
                 } else {
-                    core.info(e.title + " " + e.created_at);
+                    core.info(e.title + " " + e.created_at + " " + e.updated_at);
                 }
             }
         }
     }
-    let da = Date.parse("1970-02-01T00:00:00Z");
-    let past = Date.parse("1970-01-01T00:00:00Z");
-    let duration = da - past;
-    let millisecond = duration % 1000;
-    duration /= 1000;
-
-    let second = duration % 60;
-    duration /= 60;
-
-    let minute = duration % 60;
-    duration /= 60;
-
-    let hour = duration % 24;
-    duration /= 24;
-
-    let day = duration;
-
-    core.info(day + " " + hour + " " + minute + " " + second + " " + millisecond);
-
-    core.info(test_name);
 
 }
 
