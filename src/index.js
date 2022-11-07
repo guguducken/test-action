ac7cf193f1ce2c31c266c5a1c7c8fd1e026eccc1
guguducken <1172781832@qq.com>
2022-11-07 05:49:54 +0000

    test

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ async function run() {
                     // core.info(e.pull_request.url);
                 } else {
                     core.info(e.title);
-                    getPRTime(e);
+                    await getPRTime(e);
                 }
             }
         }
